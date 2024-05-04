9545804a3a1ac2b22cf37909f2e238a36951f385
maryam rezvani <rezvani.maryam.402@gmail.com>
2024-05-04 13:54:58 +0330

    adding bank5.py

---
+++ bank5.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, classification_report
+
